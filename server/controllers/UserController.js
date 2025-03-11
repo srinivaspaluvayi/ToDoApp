b1975e5d622062a1e5d284f3fe07582cb7192220
srinivaspaluvayi <72265945+srinivaspaluvayi@users.noreply.github.com>
2025-03-11 16:19:29 -0500

    getAndAddTasks (#13)

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -19,3 +19,43 @@ export const getUsersData = async (req, res) => {
     res.json({ message: error.message });
   }
 };
+
+export const getUserTasks = async (req, res) => {
+  try{
+    const { userId } = req.body;
+    const user = await userModel.findById(userId).exec();
+    if(user){
+      res.status(200).json({
+        tasks: user.tasks
+      });
+    }
+    else{
+      return res.status(400).json({ message: "User does not exist" });
+    }
+  }
+  catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const addUserTasks = async (req, res) => {
+  try{
+    const { userId } = req.body;
+    const user = await userModel.findById(userId).exec();
+    if(user){
+
+      const userTask = {
+        taskName: req.body.taskName
+      }
+      user.tasks.push(userTask);
+      await user.save();
+      res.status(200).redirect("/home");
+    }
+    else{
+      return res.status(400).json({ message: "User does not exist" });
+    }
+  }
+  catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+}
